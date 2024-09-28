8e2911a82b5a54c84ef50b3df3feb8c87366f34d
Candy69z <115476392+Candy69z@users.noreply.github.com>
2024-09-29 05:12:10 +0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,65 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let  searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let  cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+new Swiper('.card-wrapper', {
+    // // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    spaceBetween: 30,
+  
+    // Paganation Bullets {in-used}
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // Displaying the number of slides
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+        1200: {
+            slidesPerView: 4
+        },
+    }
+  
+  });
